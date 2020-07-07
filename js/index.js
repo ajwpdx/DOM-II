30627cac708f141c21a684d735b1d5bed930394e
Alex Williams <alexjwilliams1@gmail.com>
2020-07-07 14:42:25 -0700

    getting started

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+console.log('hi')
+
+const headerScroll = document.querySelector('.main-navigation')
+
+headerScroll.addEventListener('mouseover', () => {
+    console.log('hello')
+})
