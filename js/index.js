512f175af0ef143855dca341e8ff2477abbca0a5
Alex Williams <alexjwilliams1@gmail.com>
2020-07-07 17:10:14 -0700

    added some more event elements

--- js/index.js
+++ js/index.js
@@ -11,10 +11,32 @@ navScroll.forEach(link =>{
 })
 })
 
-//2. click: 
+//2. click: logo changes to "Buf Sun" when clicked
 
 const logoChange = document.querySelector ('.logo-heading')
 
 logoChange.addEventListener('click', () => {
     logoChange.textContent = "Buf Sun"    
 })
+
+//3. scroll: change text color to red when scrolled
+
+const bodyColor = document.querySelector ('body')
+
+document.addEventListener('scroll', () => bodyColor.style.color = 'red')
+
+
+//4. keydown:  toggle the logo on and off when pressing the "Escape" key
+document.addEventListener('keydown', function (event){
+    if (event.key === 'Escape'){
+        logoChange.classList.toggle('off')    
+    }
+})
+
+//5. doubleclick: images disapear when double clicked
+
+const imgSelector = document.querySelector('.intro img')
+
+document.addEventListener('dblclick', function (event){
+           imgSelector.classList.toggle('off') 
+    })
